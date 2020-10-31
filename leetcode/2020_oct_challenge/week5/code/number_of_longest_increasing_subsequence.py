f2c26fa28e579476cb708d57b24f30266cd36df1
Sang Bin (Kevin) Lee <kevinlt@hotmail.com>
2020-10-30 23:15:26 -0400

    Add leet code, 2020 oct challange, week 5, number of longest increasing subsequence

---
+++ leetcode/2020_oct_challenge/week5/code/number_of_longest_increasing_subsequence.py
@@ -0,0 +1,72 @@
+# Could not solve
+# Solution https://leetcode.com/problems/number-of-longest-increasing-subsequence/solution/
+
+# 2) Segment Tree O(NlogN) time O(N) space
+class Node(object):
+    def __init__(self, start, end):
+        self.range_left, self.range_right = start, end
+        self._left = self._right = None
+        self.val = 0, 1 #length, count
+    @property
+    def range_mid(self):
+        return (self.range_left + self.range_right) / 2
+    @property
+    def left(self):
+        self._left = self._left or Node(self.range_left, self.range_mid)
+        return self._left
+    @property
+    def right(self):
+        self._right = self._right or Node(self.range_mid + 1, self.range_right)
+        return self._right
+
+def merge(v1, v2):
+    if v1[0] == v2[0]:
+        if v1[0] == 0: return (0, 1)
+        return v1[0], v1[1] + v2[1]
+    return max(v1, v2)
+
+def insert(node, key, val):
+    if node.range_left == node.range_right:
+        node.val = merge(val, node.val)
+        return
+    if key <= node.range_mid:
+        insert(node.left, key, val)
+    else:
+        insert(node.right, key, val)
+    node.val = merge(node.left.val, node.right.val)
+
+def query(node, key):
+    if node.range_right <= key:
+        return node.val
+    elif node.range_left > key:
+        return 0, 1
+    else:
+        return merge(query(node.left, key), query(node.right, key))
+
+# 1) DP O(N^2) Time, O(N) space
+class Solution(object):
+    def findNumberOfLISDP(self, nums):
+        N = len(nums)
+        if N <= 1: return N
+        lengths = [0] * N #lengths[i] = longest ending in nums[i]
+        counts = [1] * N #count[i] = number of longest ending in nums[i]
+
+        for j, num in enumerate(nums):
+            for i in range(j):
+                if nums[i] < nums[j]:
+                    if lengths[i] >= lengths[j]:
+                        lengths[j] = 1 + lengths[i]
+                        counts[j] = counts[i]
+                    elif lengths[i] + 1 == lengths[j]:
+                        counts[j] += counts[i]
+
+        longest = max(lengths)
+        return sum(c for i, c in enumerate(counts) if lengths[i] == longest)
+
+    def findNumberOfLISSegmentTree(self, nums):
+        if not nums: return 0
+        root = Node(min(nums), max(nums))
+        for num in nums:
+            length, count = query(root, num-1)
+            insert(root, num, (length+1, count))
+        return root.val[1]
